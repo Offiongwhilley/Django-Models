16b68a736a34f5c8617a1f496c3dc6cb6f9ed98c
Offiongwhilley <offiongwhilley@gmail.com>
2022-06-17 15:00:24 +0100

    modified models.py

--- I4G005249YDA/blog/models.py
+++ I4G005249YDA/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 
@@ -6,7 +7,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    # author = models.ForeignKey(get_user_model())
-    created_date = models.DateTimeField()
-    published_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     
